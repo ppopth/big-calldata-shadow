78c8c3bbeefe3adc2bb1f2e2698cb9903502d561
Suphanat Chunhapanya <haxx.pop@gmail.com>
2023-02-02 18:32:20 +0700

    Deploy the contract and update bytes many times
    We do that to increase the chance that the contract and the bytes update
    transaction will be on chain more quickly.

--- run.sh
+++ run.sh
@@ -17,41 +17,54 @@ HTTP_PORT=3013
 SHADOW_CONFIG_FILE=$ROOT/shadow.yaml
 SHADOW_DIR=$ROOT/shadow
 SECONDS_PER_SLOT=$(yq .SECONDS_PER_SLOT $ROOT/consensus/config.yaml)
+STOP_TIME=$(yq .general.stop_time $SHADOW_CONFIG_FILE)
 
 CONTRACT_DEPLOY_STARTTIME=$START_TIME
 UPDATE_STARTTIME=$(expr $CONTRACT_DEPLOY_STARTTIME + $SECONDS_PER_SLOT \* 5)
 
-# Allow insecure HTTP to node1
-yq -i ".hosts.node1.processes[0].args += \"--http --http.port $HTTP_PORT --allow-insecure-unlock\"" $SHADOW_CONFIG_FILE
-echo "Enabled HTTP endpoint of node1"
+# Allow insecure HTTP to every node
+for node in $(seq 1 $NODE_COUNT); do
+    yq -i ".hosts.node$node.processes[0].args += \"--http --http.port $HTTP_PORT --allow-insecure-unlock\"" $SHADOW_CONFIG_FILE
+    echo "Enabled HTTP endpoint of node$node"
+done
 
-# Deploy the contract
-env="NODE_PATH=$(realpath ./node_modules)"
-args="$(realpath ./src/deploy-contract.js) \
+# Deploy the contract in every node so that the contract will be deployed quickly
+for node in $(seq 1 $NODE_COUNT); do
+    env="NODE_PATH=$(realpath ./node_modules)"
+    args="$(realpath ./src/deploy-contract.js) \
 --endpoint http://localhost:$HTTP_PORT \
 --build-dir $(realpath ./build) \
 --address-out $(realpath $ROOT/contract-address)"
-yq -i ".hosts.node1.processes += { \
-    \"path\": \"node\", \
-    \"environment\": \"$env\", \
-    \"args\": \"$args\", \
-    \"start_time\": $CONTRACT_DEPLOY_STARTTIME \
-}" $SHADOW_CONFIG_FILE
-echo "Added contract deploy process in node1"
+    yq -i ".hosts.node$node.processes += { \
+        \"path\": \"node\", \
+        \"environment\": \"$env\", \
+        \"args\": \"$args\", \
+        \"start_time\": $CONTRACT_DEPLOY_STARTTIME \
+    }" $SHADOW_CONFIG_FILE
+    echo "Added contract deploy process in node$node"
+done
+
+# The math here is to make sure that there will be at least one bytes update
+# with high probability.
+TIME_LEFT=$(expr $STOP_TIME - $UPDATE_STARTTIME)
+SLOTS_LEFT=$(expr $TIME_LEFT / $SECONDS_PER_SLOT + 1)
+UPDATE_NODE_COUNT=$(expr $NODE_COUNT \* 2 / $SLOTS_LEFT + 1)
 
 # Update the bytes in the contract
-env="NODE_PATH=$(realpath ./node_modules)"
-args="$(realpath ./src/update.js) \
+for node in $(seq 1 $UPDATE_NODE_COUNT); do
+    env="NODE_PATH=$(realpath ./node_modules)"
+    args="$(realpath ./src/update.js) \
 --endpoint http://localhost:$HTTP_PORT \
 --build-dir $(realpath ./build) \
 --address-file $(realpath $ROOT/contract-address) \
 --length 2097152"
-yq -i ".hosts.node1.processes += { \
-    \"path\": \"node\", \
-    \"environment\": \"$env\", \
-    \"args\": \"$args\", \
-    \"start_time\": $UPDATE_STARTTIME \
-}" $SHADOW_CONFIG_FILE
-echo "Added bytes update process in node1"
+    yq -i ".hosts.node$node.processes += { \
+        \"path\": \"node\", \
+        \"environment\": \"$env\", \
+        \"args\": \"$args\", \
+        \"start_time\": $UPDATE_STARTTIME \
+    }" $SHADOW_CONFIG_FILE
+    echo "Added bytes update process in node$node"
+done
 
 shadow -p $PARALLELISM -d $SHADOW_DIR $SHADOW_CONFIG_FILE --use-memory-manager false --progress true > $ROOT/shadow.log
