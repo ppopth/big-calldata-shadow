6b3c2e1b49d22bcd7f8ff2a8cec74b3bf0f5847e
Suphanat Chunhapanya <haxx.pop@gmail.com>
2023-02-10 13:45:28 +0700

    Add another range for histogram script

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -20,13 +20,15 @@ for node in nodes:
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.set_title(f'{sys.argv[3]}')
 high=max(times)
-if high > 4:
+if high > 8:
     ax.set_xlabel('time in second, 1s/bin')
 else:
     ax.set_xlabel('time in second, 50ms/bin')
 ax.set_ylabel('number of nodes')
-if high > 4:
+if high > 8:
     ax.hist(times, np.arange(0, 90, 1))
+elif high > 4:
+    ax.hist(times, np.arange(0, 8, 0.05))
 else:
     ax.hist(times, np.arange(0, 4, 0.05))
 fig.savefig(f'histogram.png')
