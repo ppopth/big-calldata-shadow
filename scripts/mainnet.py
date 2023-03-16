51f9d188e70793873a8b8a1df88d05d4c80e221a
Suphanat Chunhapanya <haxx.pop@gmail.com>
2023-03-16 22:20:47 +0700

    Decorate mainnet block propagation time histogram

--- scripts/mainnet.py
+++ scripts/mainnet.py
@@ -17,6 +17,9 @@ fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.set_title(f'Propagation times in mainnet observed in different locations')
 ax.set_xlabel('time in second')
 ax.set_ylabel('probability density function (pdf)')
+ax.set_xticks(np.arange(0, 10, 1.0));
+ax.set_xticks(np.arange(0, 10, 0.5), minor=True);
+all_delays = []
 for fname in sys.argv[1:]:
     f = open(fname)
     delays = []
@@ -30,6 +33,8 @@ for fname in sys.argv[1:]:
         delays.append(time.timestamp() - slot_time)
         print(slot)
     ax.hist(delays, np.arange(0, 10, 0.05), density=True, histtype='step', label=fname)
+    all_delays += delays
 
+ax.axvline(np.median(all_delays), color='k', label="median", linestyle='dashed', linewidth=1)
 ax.legend()
 fig.savefig(f'histogram.png')
